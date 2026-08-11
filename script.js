43f8ef702a7250bebff4ef6e4260149ef57e408d
uzairejaz150-hue <uzairejaz150@gmail.com>
2026-08-11 04:22:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Saudi Arabia Website Loaded");
+console.log("Saudi Arabia Tourism Website Loaded");
+
+
+/* CONTACT FORM */
+
+const contactForm = document.querySelector(".contact-form");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        alert(
+            "Thank you for contacting Saudi Arabia Tourism!"
+        );
+
+        contactForm.reset();
+
+    });
+
+}
